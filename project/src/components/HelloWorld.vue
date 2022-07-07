e87c7865b977d45635d2fc56e55455ae532cadbe
WhalenSITHS <38401651+WhalenSITHS@users.noreply.github.com>
2022-07-07 11:21:12 -0400

    m

---
+++ project/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h3>lshkbfed</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+    props: {
+        msg: String,
+    },
+    data() {
+        return {
+            name: 'lshkbfed',
+        };
+    },
+};
+</script>
+s
+<style lang="scss" scoped></style>
