1c1006f2b3e6ee86e4d721fd5838de618ffc2c99
WhalenSITHS <38401651+WhalenSITHS@users.noreply.github.com>
2022-07-07 12:11:33 -0400

    giving up on eslint for now

--- project/.eslintrc.js
+++ project/.eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
+    root: true,
+    env: { node: true },
+    // https://github.com/vuejs/vue-eslint-parser#parseroptionsparser
+    parser: 'vue-eslint-parser',
     extends: ['plugin:vue/vue3-recommended', 'prettier'],
     rules: {
         // override/add rules settings here, such as:
--- project/src/components/HelloWorld.vue
+++ project/src/components/HelloWorld.vue
@@ -12,7 +12,7 @@ export default {
     },
     data() {
         return {
-            name: 'lshkbfed',
+            name: 'lshed',
         };
     },
 };
